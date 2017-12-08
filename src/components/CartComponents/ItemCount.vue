bd9b41059f374e520c384d211262d697c5425c67
Hoisan Alex <hoisan_alex@yahoo.com>
2017-12-08 22:57:36 +0100

    Splited the cart in more components and added functionality to all the cart buttons

---
+++ src/components/CartComponents/ItemCount.vue
@@ -0,0 +1,48 @@
+<template>
+	<td>
+	  <span>{{ cart[index].count }}</span>
+	  <div class='add-remove-buttons'>
+		<button class='plus-item' :data-name='cart[index].name' @click="addItemToCart(cart[index].name, 0, 1)">+</button>
+		<button class='substract-item' :data-name='cart[index].name' @click="removeItemFromCart(cart[index].name)"> - </button>
+	  </div>
+	</td>
+</template>
+
+<script>
+	import FoodModals from './../FoodModals.vue'
+	import ItemCart from './../ItemCart.vue'
+	
+	export default {
+		props: {
+			cart: {
+				type: Array
+			},			
+			index: {
+				default: 0
+			}
+		},
+		methods: {
+			removeItemFromCart(name) {
+				console.log('remove');
+				for (let i = 0; i < this.cart.length; i++) {
+					if (this.cart[i].name === name) {
+						this.cart[i].count--;
+						if (this.cart[i].count == 0) {
+							this.cart.splice(i, 1);
+						}
+						break;
+					}
+				}
+			},
+			addItemToCart(name, price, count) {
+				for (var i in this.cart) {
+					if (this.cart[i].name === name) {
+						this.cart[i].count += count;
+						return;
+					}
+				}
+				this.cart.push(item);
+			}
+		}
+	}
+</script>
